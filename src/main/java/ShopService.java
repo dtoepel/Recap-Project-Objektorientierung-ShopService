65356f6d75e45b4d44430cdbe5f912b350416c4a
Daniel Töpel <toepel@gmx.it>
2025-07-16 10:34:09 +0200

    added Exception

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -6,13 +6,12 @@ public class ShopService {
     private final ProductRepo productRepo = new ProductRepo();
     private final OrderRepo orderRepo = new OrderMapRepo();
 
-    public Order addOrder(List<String> productIds) {
+    public Order addOrder(List<String> productIds) throws ProductNotFoundException{
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
             Product productToOrder = productRepo.getProductById(productId);
             if (productToOrder == null) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
-                return null;
+                throw new ProductNotFoundException("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
             }
             products.add(productToOrder);
         }
